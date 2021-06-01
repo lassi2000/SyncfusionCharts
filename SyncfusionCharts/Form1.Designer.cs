f396cf521dde5931f8c4ee520f6be9e672e96c4f
Lasindu Weerasinghe <lasindu.weerasinghe@qualitapps.com>
2021-06-01 13:35:13 +0100

    first commit

---
+++ SyncfusionCharts/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace SyncfusionCharts
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cChartData_GRID = new System.Windows.Forms.DataGridView();
+            this.cColLabel_COL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cValueCol_COL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cChartType_CB = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cChartData_GRID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 579);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1097, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cChartData_GRID
+            // 
+            this.cChartData_GRID.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.cChartData_GRID.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cColLabel_COL,
+            this.cValueCol_COL});
+            this.cChartData_GRID.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cChartData_GRID.Location = new System.Drawing.Point(0, 28);
+            this.cChartData_GRID.Name = "cChartData_GRID";
+            this.cChartData_GRID.RowHeadersWidth = 51;
+            this.cChartData_GRID.RowTemplate.Height = 29;
+            this.cChartData_GRID.Size = new System.Drawing.Size(1097, 551);
+            this.cChartData_GRID.TabIndex = 1;
+            // 
+            // cColLabel_COL
+            // 
+            this.cColLabel_COL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.cColLabel_COL.HeaderText = "Label";
+            this.cColLabel_COL.MinimumWidth = 6;
+            this.cColLabel_COL.Name = "cColLabel_COL";
+            // 
+            // cValueCol_COL
+            // 
+            this.cValueCol_COL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.cValueCol_COL.HeaderText = "Value";
+            this.cValueCol_COL.MinimumWidth = 6;
+            this.cValueCol_COL.Name = "cValueCol_COL";
+            // 
+            // cChartType_CB
+            // 
+            this.cChartType_CB.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cChartType_CB.FormattingEnabled = true;
+            this.cChartType_CB.Items.AddRange(new object[] {
+            "Line",
+            "Bar_Clustered",
+            "Pie"});
+            this.cChartType_CB.Location = new System.Drawing.Point(0, 0);
+            this.cChartType_CB.Name = "cChartType_CB";
+            this.cChartType_CB.Size = new System.Drawing.Size(1097, 28);
+            this.cChartType_CB.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 628);
+            this.Controls.Add(this.cChartData_GRID);
+            this.Controls.Add(this.cChartType_CB);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cChartData_GRID)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView cChartData_GRID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cColLabel_COL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cValueCol_COL;
+        private System.Windows.Forms.ComboBox cChartType_CB;
+    }
+}
+
---
+++ SyncfusionCharts/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.OfficeChart;
+
+namespace SyncfusionCharts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            cChartData_GRID.Columns[1].ValueType = typeof(double);
+
+        }
+
+
+        // get data from table
+        private object[][] GetData()
+        {
+            var rowsCount = cChartData_GRID.Rows.Count - 1;
+
+            if (rowsCount == 1) return null;
+
+            object[][] ret = new object[rowsCount + 1][];
+
+            ret[0] = new object[] { "", "Sales" };
+
+            for (int i = 1; i <= rowsCount; i++)
+            {
+                var gridRowCount = i - 1;
+
+                ret[i] = new object[2];
+                ret[i][0] = cChartData_GRID.Rows[gridRowCount].Cells[0].Value;
+                ret[i][1] = cChartData_GRID.Rows[gridRowCount].Cells[1].Value;
+            }
+
+            return ret;
+        }
+
+
+        // get chart type
+        private OfficeChartType GetChartType()
+        {
+            return Enum.Parse<OfficeChartType>(cChartType_CB.Text, true);
+        }
+
+
+        // Generate Button - Click
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string DOC_FILE = Path.Combine(Environment.CurrentDirectory, "Sample.docx");
+
+            WordDocument doc = new();
+            var para = doc.AddSection().AddParagraph();
+
+            var data = GetData();
+            var labelsCount = data.Count() - 1;
+            if (data == null)
+            {
+                MessageBox.Show("Insert some data please");
+                return;
+            }
+            var chart = para.AppendChart(data, 400, 600);
+
+            try
+            {
+                chart.ChartType = Enum.Parse<OfficeChartType>(cChartType_CB.Text, true);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please select a valid Chart Type");
+                return;
+            }
+
+            chart.ChartTitle = "Custom Chart";
+
+            var series = chart.Series[0];
+            series.Values = chart.ChartData[2, 2, labelsCount + 1, 2];
+
+            series.DataPoints.DefaultDataPoint.DataLabels.IsValue = true;
+            if (chart.ChartType == OfficeChartType.Line)
+                series.DataPoints.DefaultDataPoint.DataLabels.Position = OfficeDataLabelPosition.Below;
+            else
+                series.DataPoints.DefaultDataPoint.DataLabels.Position = OfficeDataLabelPosition.Outside;
+
+            chart.ChartArea.Fill.ForeColor = Color.FromArgb(242, 242, 242);
+            chart.PlotArea.Fill.ForeColor = Color.FromArgb(242, 242, 242);
+            chart.ChartArea.Border.LinePattern = OfficeChartLinePattern.None;
+
+            chart.PrimaryCategoryAxis.CategoryLabels = chart.ChartData[2, 1, labelsCount + 1, 1];
+
+            doc.Save(DOC_FILE);
+
+            doc.Close();
+
+            using var p = new Process();
+            p.StartInfo = new ProcessStartInfo(DOC_FILE)
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+
+        }
+
+    }
+}
